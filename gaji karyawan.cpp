1513b590deb640011a82514fa3e3141c317f3063
VIKTORKEVIN <46395918+VIKTORKEVIN@users.noreply.github.com>
2019-01-05 18:42:18 +0700

    Add files via upload

---
+++ gaji karyawan.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int gaji;
+    int jam;
+    char nama;
+    cout<<"Program input gaji karyawan \n"<<endl;
+    cout<<"gaji karyawan rp.5000 perjam dalam kerja 8 jam"<<endl;
+    cout<<"Masukkan nama karyawan=";
+    cin>>nama;
+    cout<<"-----------------"<<endl;
+        gaji=8*5000;
+    cout<<"total gaji anda=";
+    cout<<"rp."<<gaji<<endl;
+    cout<<"-----TERIMA KASIH-----"<<endl;
+}
